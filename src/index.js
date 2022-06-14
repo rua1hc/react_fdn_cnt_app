dd37522ef44e16853023231f7aae29cfe8f1ce8c
rua1hc <tatut90@gmail.com>
2022-06-14 21:47:36 +0700

    sec3vid5 emb expression

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.css";
+
+import Counter from "./comps/counter";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        <Counter />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
