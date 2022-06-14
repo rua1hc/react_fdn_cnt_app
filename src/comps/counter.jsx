8542b31c2fb54fdcdf77671a183dab3448295ca6
rua1hc <tatut90@gmail.com>
2022-06-14 22:04:13 +0700

    s3v6 setting attr

--- src/comps/counter.jsx
+++ src/comps/counter.jsx
@@ -3,13 +3,32 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count: 0,
+
+        imgUrl: "https://picsum.photos/200",
     };
+
+    // styles = {
+    //     fontSize: 20,
+    //     fontWeight: "bold",
+    // };
+
     render() {
         return (
             <React.Fragment>
+                {/* <img src={this.state.imgUrl} alt="" /> */}
+
                 {/* <span>Sum: {this.state.count}</span> */}
-                <span>Sum: {this.formatCount()}</span>
-                <button>Increment</button>
+                {/* <span style={this.styles} className="badge bg-primary m-2">
+                    Sum: {this.formatCount()}
+                </span> */}
+                {/* <span style={{ fontSize: 20 }} className="badge bg-primary m-2">
+                    Sum: {this.formatCount()}
+                </span> */}
+                <span className="badge bg-primary m-2">
+                    Sum: {this.formatCount()}
+                </span>
+
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
